c6b7fe4d61511d7bbf3d678ba9f8e476dc4b84ab
unknown <juhasztamas.hatico@gmail.com>
2021-11-16 13:16:08 +0100

    meszámlálás tétele

--- PythonApplication1/PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1/PythonApplication1.py
@@ -1,16 +1,22 @@
 print("Működik a Git?")
 print("Mindjárt kiderül!")
 
-
+print("_____________________")
 szamok=[3,4,2,7,8,1,9,7,3]
 osszeg=0
 for x in szamok:
     osszeg=osszeg+x
     print(osszeg)
 print(osszeg)
-
+print("_____________________")
 vane =False
 for x in szamok:
     if (x==1):
         vane=True
 print(vane)
+print("_____________________")
+db=0
+for x in  szamok:
+    if (x==3):
+        db=db+1;
+print(db)
